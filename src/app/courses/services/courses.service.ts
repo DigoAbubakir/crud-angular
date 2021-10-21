08defaea1146bddea2701a10759b29f7db1c1f48
Warley <warleyvods@gmaill.com>
2021-10-21 18:05:21 -0300

    adicionando service e http client

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Course} from "../model/course";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {first} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = 'localhost:8080'
+
   constructor(private httpClient: HttpClient) {
   }
 
-  list(): Course[] {
-    return [
-      { _id: 1, name: 'Angular', category: 'Front-end' }
-    ];
+  list(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.API).pipe(first());
   }
 
 }
