42ea18528225938f56959ae50fc8d4c6c9a3c3c3
Warley <warleyvods@gmaill.com>
2021-10-21 17:31:17 -0300

    adicionando shared module

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,14 +3,11 @@ import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from "@angular/material/toolbar";
 
-
-
 @NgModule({
   exports: [
     MatTableModule,
     MatCardModule,
     MatToolbarModule
-  ],
-
+  ]
 })
 export class AppMaterialModule { }
