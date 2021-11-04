82b545372a7439c475d6529f2e94d1eb7886a34f
Rodrigo <digosantosabubakir@gmail.com>
2021-11-04 19:21:59 -0300

    adicionando sniper no projeto

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-courses',
@@ -9,11 +10,12 @@ import {CoursesService} from "../services/courses.service";
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = this.coursesService.list()
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name', 'category']
 
   constructor(private coursesService: CoursesService) {
+    this.courses$ = this.coursesService.list()
   }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,25 @@
 import {Injectable} from '@angular/core';
 import {Course} from "../model/course";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {delay, first, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = 'http://localhost:9000'
+
   constructor(private httpClient: HttpClient) {
   }
 
-  list(): Course[] {
-    return [
-      { _id: 1, name: 'Angular', category: 'Front-end' }
-    ];
+  list(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.API + '/courses')
+      .pipe(
+        first(),
+        delay(0)
+      );
   }
 
 }
