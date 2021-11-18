9cf6512776b7493ca85cf7176dfe28398843be46
Rodrigo <digosantosabubakir@gmail.com>
2021-11-18 20:00:52 -0300

    adicionando botoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        HttpClientModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,17 +5,19 @@ import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import {SharedModule} from "../shared/shared.module";
+import {MatMenuModule} from "@angular/material/menu";
 
 
 @NgModule({
   declarations: [
     CoursesComponent
   ],
-  imports: [
-    CommonModule,
-    CoursesRoutingModule,
-    AppMaterialModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        CoursesRoutingModule,
+        AppMaterialModule,
+        SharedModule,
+        MatMenuModule
+    ]
 })
 export class CoursesModule { }
